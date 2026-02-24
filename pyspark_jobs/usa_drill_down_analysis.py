893d4661c2684f631d4e2d96878f515f08079013
Madhumitha212 <madhuravi324@gmail.com>
2026-02-24 07:21:20 +0530

    update: [Madhu] updated program which analyse usa covid data

--- pyspark_jobs/usa_drill_down_analysis.py
+++ pyspark_jobs/usa_drill_down_analysis.py
@@ -1,3 +1,10 @@
+# Task 7: USA Drilldown Analysis
+
+# Aggregate county data to state level.
+# Identify top 10 affected states.
+# Detect data skew across states.
+# Explain skew impact in distributed systems.
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.window import Window
@@ -24,22 +31,29 @@ aggregate_df = usa_df.groupBy("Province_State","Country_Region")\
          sum("Confirmed").alias("Total_affected"),\
          sum("Deaths").alias("Total_deaths")
         )
+aggregate_df.write.mode("overwrite") \
+    .parquet(analytics_path + "aggregate_county_data.parquet")
+
 aggregate_df.show()
 
 # 2.Identify top 10 affected states.
 affected_df = aggregate_df.orderBy(col("Total_affected").desc())\
-             .select("Province_State", "Country_Region","Total_affected")
-affected_df.show(10)
+             .select("Province_State", "Country_Region","Total_affected").limit(10)
+affected_df.write.mode("overwrite") \
+    .parquet(analytics_path + "top_10_affected_states.parquet")
+
+affected_df.show()
 
 # 3.Detect data skew across states.
-
 state_distribution = usa_df.groupBy("Province_State") \
     .agg(count("*").alias("record_count")) \
     .orderBy("record_count", ascending=False)
 
-state_distribution.show()
+state_distribution = state_distribution.describe()
+state_distribution.write.mode("overwrite") \
+    .parquet(analytics_path + "data_scew_across_states.parquet")
 
-state_distribution.describe().show()
+state_distribution.show()
 
 # 4.Explain skew impact in distributed systems.
 # Data skew occurs when some keys contain significantly more records than others,
